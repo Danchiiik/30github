85dbee673f4684ae9b1920054fed268740c6b3c7
Danchiiik <dcabatar@gmail.com>
2025-02-21 16:48:21 +0600

    urls

--- apps/cards/urls.py
+++ apps/cards/urls.py
@@ -1,1 +1,18 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from apps.cards.views import CardsViewSet
+
+from . import views
+
+
+router = DefaultRouter()
+
+router.register('', CardsViewSet)
+
+
+
+urlpatterns = [
+    path('api/v1/cards/', include(router.urls))
+]
+
--- apps/cards/views.py
+++ apps/cards/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.viewsets import mixins, GenericViewSet
 
 from apps.cards.models import Cards
+from apps.cards.permissions import CardsPermissions
 from apps.cards.serializers import CardsSerializer
 
 
@@ -14,4 +15,4 @@ class CardsViewSet(mixins.CreateModelMixin,
                    GenericViewSet):
     queryset = Cards
     serializer_class = CardsSerializer
-    permission_classes = []
+    permission_classes = [CardsPermissions]
