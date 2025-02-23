888c37c0a78d6a07b697dbd5f8e93c22e8850db3
Danchiiik <dcabatar@gmail.com>
2025-02-24 02:04:57 +0600

    serializer

--- apps/cards/models.py
+++ apps/cards/models.py
@@ -22,13 +22,195 @@ STATUS_OF_STUDIES = (
     ('Finished', 'Finished')
 )
 
+WANTED_DEGREES = (
+    # Technology & Engineering
+    ('Software Engineering', 'Software Engineering'),
+    ('Computer Science', 'Computer Science'),
+    ('Information Technology', 'Information Technology'),
+    ('Cybersecurity', 'Cybersecurity'),
+    ('Data Science', 'Data Science'),
+    ('Artificial Intelligence', 'Artificial Intelligence'),
+    ('Machine Learning', 'Machine Learning'),
+    ('Cloud Computing', 'Cloud Computing'),
+    ('Network Engineering', 'Network Engineering'),
+    ('Game Development', 'Game Development'),
+    ('Mobile App Development', 'Mobile App Development'),
+    ('Web Development', 'Web Development'),
+    ('Computer Engineering', 'Computer Engineering'),
+    ('Electrical Engineering', 'Electrical Engineering'),
+    ('Mechanical Engineering', 'Mechanical Engineering'),
+    ('Civil Engineering', 'Civil Engineering'),
+    ('Biomedical Engineering', 'Biomedical Engineering'),
+    ('Robotics Engineering', 'Robotics Engineering'),
+    ('Aerospace Engineering', 'Aerospace Engineering'),
+    ('Automotive Engineering', 'Automotive Engineering'),
+    ('Nanotechnology', 'Nanotechnology'),
+
+    # Business & Finance
+    ('Business Administration', 'Business Administration'),
+    ('Entrepreneurship', 'Entrepreneurship'),
+    ('Finance', 'Finance'),
+    ('Accounting', 'Accounting'),
+    ('Economics', 'Economics'),
+    ('Marketing', 'Marketing'),
+    ('Human Resource Management', 'Human Resource Management'),
+    ('Supply Chain Management', 'Supply Chain Management'),
+    ('Business Analytics', 'Business Analytics'),
+    ('International Business', 'International Business'),
+    ('Project Management', 'Project Management'),
+
+    # Medicine & Health Sciences
+    ('Medicine', 'Medicine'),
+    ('Dentistry', 'Dentistry'),
+    ('Pharmacy', 'Pharmacy'),
+    ('Nursing', 'Nursing'),
+    ('Physiotherapy', 'Physiotherapy'),
+    ('Public Health', 'Public Health'),
+    ('Veterinary Medicine', 'Veterinary Medicine'),
+    ('Biotechnology', 'Biotechnology'),
+    ('Psychology', 'Psychology'),
+    ('Nutrition & Dietetics', 'Nutrition & Dietetics'),
+
+    # Natural & Environmental Sciences
+    ('Biology', 'Biology'),
+    ('Chemistry', 'Chemistry'),
+    ('Physics', 'Physics'),
+    ('Mathematics', 'Mathematics'),
+    ('Environmental Science', 'Environmental Science'),
+    ('Geology', 'Geology'),
+    ('Meteorology', 'Meteorology'),
+    ('Oceanography', 'Oceanography'),
+    ('Astronomy', 'Astronomy'),
+
+    # Social Sciences & Humanities
+    ('Political Science', 'Political Science'),
+    ('International Relations', 'International Relations'),
+    ('Law', 'Law'),
+    ('Criminal Justice', 'Criminal Justice'),
+    ('Sociology', 'Sociology'),
+    ('Anthropology', 'Anthropology'),
+    ('History', 'History'),
+    ('Philosophy', 'Philosophy'),
+    ('Linguistics', 'Linguistics'),
+
+    # Art, Media & Design
+    ('Fine Arts', 'Fine Arts'),
+    ('Graphic Design', 'Graphic Design'),
+    ('Animation', 'Animation'),
+    ('Film Production', 'Film Production'),
+    ('Photography', 'Photography'),
+    ('Music', 'Music'),
+    ('Theater & Performing Arts', 'Theater & Performing Arts'),
+    ('Fashion Design', 'Fashion Design'),
+    ('Interior Design', 'Interior Design'),
+    ('Architecture', 'Architecture'),
+
+    # Aviation & Maritime
+    ('Aviation Management', 'Aviation Management'),
+    ('Pilot Training', 'Pilot Training'),
+    ('Aeronautical Engineering', 'Aeronautical Engineering'),
+    ('Marine Engineering', 'Marine Engineering'),
+    ('Naval Architecture', 'Naval Architecture'),
+    ('Logistics & Transportation', 'Logistics & Transportation'),
+
+    # Education
+    ('Primary Education', 'Primary Education'),
+    ('Secondary Education', 'Secondary Education'),
+    ('Special Education', 'Special Education'),
+    ('Educational Leadership', 'Educational Leadership'),
+    ('TESOL (Teaching English as a Second Language)', 'TESOL'),
+
+    # Hospitality & Tourism
+    ('Hospitality Management', 'Hospitality Management'),
+    ('Tourism Management', 'Tourism Management'),
+    ('Culinary Arts', 'Culinary Arts'),
+    ('Event Management', 'Event Management'),
+
+    # Sports & Recreation
+    ('Sports Science', 'Sports Science'),
+    ('Physical Education', 'Physical Education'),
+    ('Sports Management', 'Sports Management'),
+
+    # Miscellaneous
+    ('Ethical Hacking', 'Ethical Hacking'),
+    ('Game Design', 'Game Design'),
+    ('Forensic Science', 'Forensic Science'),
+    ('Archaeology', 'Archaeology'),
+    ('Library & Information Science', 'Library & Information Science'),
+)
+
+WANTED_COUNTRIES = (
+    ('United States', 'United States'),
+    ('Canada', 'Canada'),
+    ('United Kingdom', 'United Kingdom'),
+    ('Australia', 'Australia'),
+    ('Germany', 'Germany'),
+    ('France', 'France'),
+    ('Spain', 'Spain'),
+    ('Italy', 'Italy'),
+    ('Netherlands', 'Netherlands'),
+    ('Sweden', 'Sweden'),
+    ('Norway', 'Norway'),
+    ('Denmark', 'Denmark'),
+    ('Finland', 'Finland'),
+    ('Switzerland', 'Switzerland'),
+    ('Austria', 'Austria'),
+    ('Belgium', 'Belgium'),
+    ('Ireland', 'Ireland'),
+    ('New Zealand', 'New Zealand'),
+    ('Japan', 'Japan'),
+    ('South Korea', 'South Korea'),
+    ('China', 'China'),
+    ('Singapore', 'Singapore'),
+    ('United Arab Emirates', 'United Arab Emirates'),
+    ('Saudi Arabia', 'Saudi Arabia'),
+    ('India', 'India'),
+    ('Brazil', 'Brazil'),
+    ('Mexico', 'Mexico'),
+    ('Argentina', 'Argentina'),
+    ('Chile', 'Chile'),
+    ('South Africa', 'South Africa'),
+    ('Russia', 'Russia'),
+    ('Ukraine', 'Ukraine'),
+    ('Poland', 'Poland'),
+    ('Portugal', 'Portugal'),
+    ('Greece', 'Greece'),
+    ('Turkey', 'Turkey'),
+    ('Czech Republic', 'Czech Republic'),
+    ('Hungary', 'Hungary'),
+    ('Malaysia', 'Malaysia'),
+    ('Thailand', 'Thailand'),
+    ('Indonesia', 'Indonesia'),
+    ('Vietnam', 'Vietnam'),
+    ('Philippines', 'Philippines'),
+    ('Egypt', 'Egypt'),
+    ('Israel', 'Israel'),
+    ('Kazakhstan', 'Kazakhstan'),
+    ('Colombia', 'Colombia'),
+    ('Peru', 'Peru'),
+    ('Pakistan', 'Pakistan'),
+    ('Bangladesh', 'Bangladesh'),
+    ('Nigeria', 'Nigeria'),
+    ('Kenya', 'Kenya'),
+    ('Morocco', 'Morocco'),
+    ('Qatar', 'Qatar'),
+    ('Kuwait', 'Kuwait'),
+    ('Oman', 'Oman'),
+    ('Lebanon', 'Lebanon'),
+    ('Kyrgyzstan', 'Kyrgyzstan'), 
+)
+
+
+class Documents(models.Model):
+    document = models.FileField(upload_to='documents/')
+
 
 class Cards(models.Model):
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
     surname = models.CharField(max_length=150)
-    image = models.FileField(upload_to='images/')  # Main thumbnail image
+    image = models.ImageField(upload_to='images/')  
 
     country = models.CharField(max_length=50)
     citizenship = models.CharField(max_length=100)
@@ -53,21 +235,13 @@ class Cards(models.Model):
     study_language = models.CharField(max_length=50)
     gpa = models.FloatField()
 
-
-    # wanted_degrees = models.CharField(choices=..., max_length=200)
-    # wanted_countries = models.CharField(choices=..., max_length=200)
+    wanted_degrees = models.CharField(choices=WANTED_DEGREES, max_length=200)
+    wanted_countries = models.CharField(choices=WANTED_COUNTRIES, max_length=200)
     financial_support = models.BooleanField(default=False)
 
-
     essay = models.CharField(max_length=5000, null=True, blank=True)
-    diploma = models.FileField()
-    documents = models.FileField()
-
-
-    
-
-    
-
+    diploma = models.FileField(upload_to='diploma/')
+    documents = models.ManyToManyField(Documents, related_name='cards')
     
 
     def __str__(self) -> str:
@@ -79,24 +253,7 @@ class Cards(models.Model):
         verbose_name_plural = 'Cards'
 
 
-# class Events(models.Model):
-#     name = models.CharField(max_length=150)
-#     image = models.FileField(upload_to='images/')  # Main thumbnail image
-#     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
-#     description = models.TextField(null=True, blank=True, max_length=2500)
-#     region = models.CharField(max_length=50, choices=REGION)
-#     address = models.CharField(max_length=100)
-#     date = models.DateField()
-#     time = models.TimeField()
-#     type_of_event = models.CharField(max_length=50, choices=TYPE_OF_EVENT)
-#     type = models.CharField(max_length=50, choices=TYPE)
-#     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-#     video = models.FileField(upload_to='videos/', null=True, blank=True)  # New field for video
-
-#     def __str__(self) -> str:
-#         return self.name
-
-#     class Meta:
-#         verbose_name = 'Мероприятие'
-#         verbose_name_plural = 'Мероприятия'
+class Documents(models.Model):
+    card = models.ForeignKey(Cards, on_delete=models.CASCADE, related_name='cards_document')
+    document = models.FileField(upload_to='documents/')
 
--- apps/cards/serializers.py
+++ apps/cards/serializers.py
@@ -1,11 +1,40 @@
 from rest_framework import serializers
 
-from apps.cards.models import Cards
+from apps.cards.models import Cards, Documents
+
+
+class DocumentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Documents
+        fields = '__all__'
 
 
 class CardsSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.email')
+    documents = DocumentSerializer(many=True, required=False)
 
     class Meta:
         model = Cards
         fields = '__all__'
+
+    def create(self, validated_data):
+        documents_data = validated_data.pop('documents', [])
+        card = Cards.objects.create(**validated_data)
+        
+        for doc_data in documents_data:
+            document = Documents.objects.create(file=doc_data['file'])
+            card.documents.add(document)
+
+        return card
+
+    def update(self, instance, validated_data):
+        documents_data = validated_data.pop('documents', None)
+
+        if documents_data is not None:
+            instance.documents.clear()
+            for doc_data in documents_data:
+                document = Documents.objects.create(file=doc_data['file'])
+                instance.documents.add(document)
+
+        return super().update(instance, validated_data)
