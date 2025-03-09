16677eaf3d42ea600ea6c51379fb741a957dcdd0
Danchiiik <dcabatar@gmail.com>
2025-03-10 02:04:00 +0600

    route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "../src/styles/reset.css";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 // import About from "./pages/About";
 // import Profile from "./pages/Profile";
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
